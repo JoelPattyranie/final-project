f45542f763cf72f36908180105a2ca58dfc0d5ae
unknown <s2200150@student.unklab.ac.id>
2022-07-02 21:43:23 +0800

    add json and get data from api

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,21 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 export default function About ()
 {
+    const [about, setAbout] = useState([])
+    useEffect(() =>
+    {
+        axios.get("http://localhost:3004/about").then(res => setAbout(res.data))
+
+    },
+    []
+    )
     return(
         <section id="about-us">
-            <div className="container">
+            {
+                about.map(item => {
+                    return(
+                        <div className="container">
             <h1 className="title text-center">  Nama </h1>
             <div className="row">
                 <div className="col-md-6 about-us">
                 <ul>
-                    <li><h1>Joel</h1></li>
-                    <li><h1>Bennett</h1></li>
-                    <li><h1>Pattyranie</h1></li>
+                    <li><h1>{item.section}</h1></li>
+                    <img src={item.image} alt="" width="200px" height="200px"/>
                 </ul>
                 </div>
                 <div className="col-md-6">
                 </div>
             </div> 
             </div>
+                    )
+                }
+                    )
+            }
         </section>
     )
 }
